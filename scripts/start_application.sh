c3301bfbd9c9f6f82989e5335c751a4000a14401
pratham1248 <mandaveprathamesh5@gmail.com>
2025-03-29 15:05:38 +0000

    v7

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Starting Flask app with gunicorn..."
 cd ..
-gunicorn --bind 0.0.0.0:80 app:app  # Replace 'app:app' with the actual entry point of your Flask app
+gunicorn --bind 0.0.0.0:8080 app:app  # Replace 'app:app' with the actual entry point of your Flask app
