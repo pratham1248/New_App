2d6a90efff6cdd9b826757052a31f979d95190aa
pratham1248 <mandaveprathamesh5@gmail.com>
2025-03-29 15:32:39 +0000

    v9

--- app.py
+++ app.py
@@ -9,5 +9,6 @@ def home():
     return "Hello, World! This is my flask application updated code123"
 
 # Run the Flask application
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
+
--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-set -e  # Exit on error
-source ~/.bash_profile
-source ~/.bashrc
-source /path/to/venv/bin/activate  # Activate virtual environment if applicable
-echo "Environment Variables:"
-env
-
 echo "Starting Flask app with gunicorn..."
 cd ..
-gunicorn --bind 0.0.0.0:8080 app:app  # Replace 'app:app' with the actual entry point of your Flask app
+python app.py
+  
