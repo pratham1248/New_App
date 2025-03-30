5a398af2f68782989e7c30c25c5deaef947c6f66
pratham1248 <mandaveprathamesh5@gmail.com>
2025-03-30 05:24:22 +0000

    alt3

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -2,6 +2,7 @@
 echo "Starting Flask app with gunicorn..."
 echo "Current working directory: $(pwd)"
 cd "$PWD"
+
 uwsgi --http :8080 --wsgi-file app.py --callable app
 #python app.py
 #gunicorn --workers 1 --bind 0.0.0.0:8080 app:app
