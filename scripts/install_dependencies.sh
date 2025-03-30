2310c4cc0c813c1ef3d53fd10824f5a7aec7a9dc
pratham1248 <mandaveprathamesh5@gmail.com>
2025-03-30 06:30:16 +0000

    final code change

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -33,7 +33,8 @@ python3.11 -m ensurepip --upgrade
 python3.11 -m pip install --upgrade pip
 python3.11 -m pip install Flask
 python3.11 -m pip install gunicorn
-python3.11 -m pip install uwsgi
+python3.11 -m pip install waitress
+
 
 
 echo "Python upgrade and setup completed!"
--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -3,7 +3,8 @@ echo "Starting Flask app with gunicorn..."
 echo "Current working directory: $(pwd)"
 cd "$PWD"
 
+python3 -m waitress --port=8080 app:app
 #uwsgi --http :8080 --wsgi-file app.py --callable app
-python app.py
+#python app.py
 #gunicorn --workers 1 --bind 0.0.0.0:8080 app:app
  # Replace 'app:app' with the actual entry point of your Flask app
