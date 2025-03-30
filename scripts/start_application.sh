20f3642f5d67077f7224cceddf98e2f6be26fe87
pratham1248 <mandaveprathamesh5@gmail.com>
2025-03-30 04:53:44 +0000

    v15

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Starting Flask app with gunicorn..."
 echo "Current working directory: $(pwd)"
-cd ..
-gunicorn --workers 1 --bind 0.0.0.0:8080 app:app
+cd "$PWD"
+gunicorn --bind 0.0.0.0:8080 app:app
  # Replace 'app:app' with the actual entry point of your Flask app
